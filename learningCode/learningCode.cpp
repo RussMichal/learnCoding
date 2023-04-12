7df1f697faa762d8e593665670af817fe552653e
Codybeard123 <Codybeard123@gmail.com>
2023-04-11 20:07:21 -0500

    Hello

--- learningCode/learningCode.cpp
+++ learningCode/learningCode.cpp
@@ -7,18 +7,4 @@
 
 int main(std::string arg)
 {
-
-	//functions
-	//multi files
-		//headers and implementation files or .h and .cpp
-		//compile?? 
-//git
-
-	int teacher1[20] = {5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5};
-	int teacher2[25];
-	int teacher3[18];
-
-	int t1avg = average(teacher1, 20);
-	int t2avg = average(teacher2, 25);
-	int t3avg = average(teacher3, 18);
 }
