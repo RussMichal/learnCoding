af13040cd4acf3094ed09f0582b08780db47e1ad
RussMichal <russell.michal@gmail.com>
2023-04-18 20:34:30 -0500

    Revert "adding assignment"
    This reverts commit 8cd69f5f89e29a28ed6eaf86c10c97fe9d09a67f.

--- learningCode/learningCode.cpp
+++ learningCode/learningCode.cpp
@@ -7,12 +7,9 @@
 
 int main(std::string arg)
 {
-	//print menu of options
-	//1. options conversion ft to in lesson1
-	//2. calculator lesson2
-	//3. factorial lesson3
-	//4. sort an array lesson4
-	//5. search an array lesson5 
-
-	// if 4 is entered then run lesson3.
+	//imani was here
+	// Russel is the best
+	asdf
+		asdf
+		asfd
 }
