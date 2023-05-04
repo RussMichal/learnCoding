38db173a2cc4c299961aa7997ba6dcc42257ba2f
RussMichal <russell.michal@gmail.com>
2023-05-03 20:21:49 -0500

    data struct lesson

--- learningCode/learningCode.cpp
+++ learningCode/learningCode.cpp
@@ -2,14 +2,49 @@
 //
 
 #include <iostream>
-#include "avg.h"
-#include "ImaniLesson5.h"
+#include<vector>
+#include<queue>
+#include<stack>
 
 int main(std::string arg)
 {
-	//imani was here
-	// Russel is the best
-	asdf
-		asdf
-		asfd
+	std::vector<int> vec;
+	vec.reserve(20);
+	for (int i = 0; i < 20; i++)
+	{
+		vec.push_back(i);
+	}
+	int cap = vec.capacity();
+	int size = vec.size();
+
+	std::vector<int> peopleInStore;
+	peopleInStore.reserve(10);
+
+	peopleInStore.push_back(20);
+	peopleInStore.push_back(10);
+	peopleInStore.push_back(100);
+	peopleInStore.erase(peopleInStore.begin());
+
+	std::queue<std::string> line;//FIFO
+	line.push("tim");
+	line.push("john");
+	line.push("jim");
+	line.push("bob");
+	line.pop();
+	line.pop();
+
+	std::stack<int> Deck;//FILO
+	Deck.push(1);
+	Deck.push(2);
+	Deck.push(3);
+	Deck.pop();
+	//data structures
+
+	//loop until - num entered
+	// add each num entered into a contianer
+	// print all values in container
+	// print avg of container	
+
+	//vector, linked lists
+	//all the rest
 }
